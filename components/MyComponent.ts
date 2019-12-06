1d36311ac1f7ebd619a639fb75371324a2497473
Luis David <38225539+lsotoangeldonis@users.noreply.github.com>
2019-12-06 12:45:37 -0500

    Create MyComponent.ts

---
+++ components/MyComponent.ts
@@ -0,0 +1,10 @@
+
+import {
+    Component
+} from 'vue-property-decorator'
+
+// The @Component decorator indicates the class is a Vue component
+@Component({})
+export default class NavbarCategoriesComponent {
+
+}
